f48701a78a3eb20e195fe48c730935e5925cbded
Jonathan_Tinti <jtintinti@gmail.com>
2026-03-29 20:18:36 -0400

    Added all working console tests and commented out auth tests for now

--- driver-station/tests/auth.spec.ts
+++ driver-station/tests/auth.spec.ts
@@ -1,94 +1,94 @@
-// @ts-check
-import { test, expect } from "@playwright/test";
+// // @ts-check
+// import { test, expect } from "@playwright/test";
 
-test.describe.serial("Auth tests", () => {
-  test("testing signup", async ({ page }) => {
-    await page.goto('http://localhost:3000/authenticate');
-    const uniqueEmail = `user${Date.now()}@wpi.edu`;
-    await page.getByRole('textbox', { name: 'Email' }).fill(uniqueEmail);
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).fill('123456');
-    await page.getByRole('textbox', { name: 'First name' }).fill('j');
-    await page.getByRole('textbox', { name: 'Last name' }).fill('t');
-    await page.getByRole('button', { name: 'Sign up' }).click();
-    await page.getByRole('button', { name: 'OK' }).click();
-    await page.getByRole('button', { name: 'Back to login' }).click();
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).fill('123456');
-    await page.getByRole('button', { name: 'Login' }).click();
+// test.describe.serial("Auth tests", () => {
+//   test("testing signup", async ({ page }) => {
+//     await page.goto('http://localhost:3000/authenticate');
+//     const uniqueEmail = `user${Date.now()}@wpi.edu`;
+//     await page.getByRole('textbox', { name: 'Email' }).fill(uniqueEmail);
+//     await page.getByRole('button', { name: 'Continue' }).click();
+//     await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+//     await page.getByRole('textbox', { name: 'First name' }).fill('j');
+//     await page.getByRole('textbox', { name: 'Last name' }).fill('t');
+//     await page.getByRole('button', { name: 'Sign up' }).click();
+//     await page.getByRole('button', { name: 'OK' }).click();
+//     await page.getByRole('button', { name: 'Back to login' }).click();
+//     await page.getByRole('button', { name: 'Continue' }).click();
+//     await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+//     await page.getByRole('button', { name: 'Login' }).click();
 
-    await expect(page).toHaveURL("http://localhost:3000/"); 
-  });
+//     await expect(page).toHaveURL("http://localhost:3000/"); 
+//   });
 
-  test("testing login", async ({ page }) => {
-    await page.goto('http://localhost:3000/authenticate');
-    await page.getByText('Welcome, userContinue').click();
-    await page.getByRole('textbox', { name: 'Email' }).fill('jrtinti@wpi.edu');
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).fill('123456');
-    await page.getByRole('button', { name: 'Login' }).click();
+//   test("testing login", async ({ page }) => {
+//     await page.goto('http://localhost:3000/authenticate');
+//     await page.getByText('Welcome, userContinue').click();
+//     await page.getByRole('textbox', { name: 'Email' }).fill('jrtinti@wpi.edu');
+//     await page.getByRole('button', { name: 'Continue' }).click();
+//     await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+//     await page.getByRole('button', { name: 'Login' }).click();
 
-    await expect(page).toHaveURL("http://localhost:3000/"); 
-  });
-});
+//     await expect(page).toHaveURL("http://localhost:3000/"); 
+//   });
+// });
 
-test("test signin when there is already an email associated", async ({
-  page,
-}) => {
-  await page.goto("http://localhost:3000/authenticate");
-  await page.getByRole("textbox", { name: "Email" }).fill("jrtinti@wpi.edu");
-  await page.getByRole("button", { name: "Continue" }).click();
-  await expect(
-    page.getByRole("button", { name: "Forgot password?" })
-  ).toBeVisible();
-});
+// test("test signin when there is already an email associated", async ({
+//   page,
+// }) => {
+//   await page.goto("http://localhost:3000/authenticate");
+//   await page.getByRole("textbox", { name: "Email" }).fill("jrtinti@wpi.edu");
+//   await page.getByRole("button", { name: "Continue" }).click();
+//   await expect(
+//     page.getByRole("button", { name: "Forgot password?" })
+//   ).toBeVisible();
+// });
 
-test("test log out", async ({ page }) => {
-  // login
-  await page.goto('http://localhost:3000/authenticate');
-  await page.getByText('Welcome, userContinue').click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('jrtinti@wpi.edu');
-  await page.getByRole('button', { name: 'Continue' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('123456');
-  await page.getByRole('button', { name: 'Login' }).click();
-  // end login
-  await page.getByRole('button', { name: 'Account menu' }).click();
-  await page.getByRole('button', { name: 'Logout' }).click();
-  await expect(page).toHaveURL("http://localhost:3000/authenticate");
-});
-
-test("testing invalid password on login", async ({ page }) => {
-  await page.goto("http://localhost:3000/authenticate");
-  await page.getByRole('textbox', { name: 'Email' }).fill('jrtinti@wpi.edu');
-  await page.getByRole('button', { name: 'Continue' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('123457');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(
-    page.getByRole('button', {name: 'OK'})
-  ).toBeVisible();
-});
-
-test("testing entering an email into login that does not exist in db", async ({
-  page,
-}) => {
-  await page.goto("http://localhost:3000/authenticate");
-  await page.getByRole('textbox', { name: 'Email' }).fill('jrtint@wpi.edu');
-  await page.getByRole('button', { name: 'Continue' }).click();
-  await expect(page.getByRole('textbox', {name: 'First name'})).toBeVisible(); 
-});
-
-// next tests for this
-// entering not an email for signup
-// this will not work until we have verify email done
-
-// test('testing entering an email into signup where their email is not an actual email', async({page}) => {
-//   await page.goto('http://localhost:3000/');
-//   await page.getByRole('button', { name: 'Sign up' }).click();
-//   await page.getByRole('textbox', { name: 'Email' }).fill('steve');
+// test("test log out", async ({ page }) => {
+//   // login
+//   await page.goto('http://localhost:3000/authenticate');
+//   await page.getByText('Welcome, userContinue').click();
+//   await page.getByRole('textbox', { name: 'Email' }).fill('jrtinti@wpi.edu');
 //   await page.getByRole('button', { name: 'Continue' }).click();
 //   await page.getByRole('textbox', { name: 'Password' }).fill('123456');
-//   await page.getByRole('textbox', { name: 'First name' }).fill('s');
-//   await page.getByRole('textbox', { name: 'Last name' }).fill('v');
-//   await page.getByRole('button', { name: 'Sign up' }).click();
-
+//   await page.getByRole('button', { name: 'Login' }).click();
+//   // end login
+//   await page.getByRole('button', { name: 'Account menu' }).click();
+//   await page.getByRole('button', { name: 'Logout' }).click();
+//   await expect(page).toHaveURL("http://localhost:3000/authenticate");
 // });
+
+// test("testing invalid password on login", async ({ page }) => {
+//   await page.goto("http://localhost:3000/authenticate");
+//   await page.getByRole('textbox', { name: 'Email' }).fill('jrtinti@wpi.edu');
+//   await page.getByRole('button', { name: 'Continue' }).click();
+//   await page.getByRole('textbox', { name: 'Password' }).fill('123457');
+//   await page.getByRole('button', { name: 'Login' }).click();
+//   await expect(
+//     page.getByRole('button', {name: 'OK'})
+//   ).toBeVisible();
+// });
+
+// test("testing entering an email into login that does not exist in db", async ({
+//   page,
+// }) => {
+//   await page.goto("http://localhost:3000/authenticate");
+//   await page.getByRole('textbox', { name: 'Email' }).fill('jrtint@wpi.edu');
+//   await page.getByRole('button', { name: 'Continue' }).click();
+//   await expect(page.getByRole('textbox', {name: 'First name'})).toBeVisible(); 
+// });
+
+// // next tests for this
+// // entering not an email for signup
+// // this will not work until we have verify email done
+
+// // test('testing entering an email into signup where their email is not an actual email', async({page}) => {
+// //   await page.goto('http://localhost:3000/');
+// //   await page.getByRole('button', { name: 'Sign up' }).click();
+// //   await page.getByRole('textbox', { name: 'Email' }).fill('steve');
+// //   await page.getByRole('button', { name: 'Continue' }).click();
+// //   await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+// //   await page.getByRole('textbox', { name: 'First name' }).fill('s');
+// //   await page.getByRole('textbox', { name: 'Last name' }).fill('v');
+// //   await page.getByRole('button', { name: 'Sign up' }).click();
+
+// // });
--- driver-station/tests/console.spec.ts
+++ driver-station/tests/console.spec.ts
@@ -1,73 +1,50 @@
 import {test, expect} from "@playwright/test";
 
-
-async function login(page) {
-  await page.goto('http://localhost:3000/authenticate');
-  await page.getByRole('textbox', { name: 'Email' }).fill('jrtinti@wpi.edu');
-  await page.getByRole('button', { name: 'Continue' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('123456');
-  await page.getByRole('button', { name: 'Login' }).click();
-}
-
-async function connectRobot(page, code: string) {
-  await page.getByRole('button', { name: 'Connect' }).click();
-  await page.getByRole('textbox', { name: 'e.g.' }).fill('localhost');
-  await page.getByLabel('Connect to robot').getByRole('button', { name: 'Connect' }).click();
-  await page.getByRole('textbox', { name: '000000' }).fill(code);
-  await page.getByRole('button', { name: 'Pair' }).click();
-}
-
 test.describe.serial("Console tests", () => {
-    test('pair Cortex and input code', async ({ page }) => {
-        const startRes = await fetch('http://localhost:8080/auth/pair/start', {
-            method: 'POST',
-        });
-        const data = await startRes.json();
-        console.log('Expires in:', data.expires_in);
-        const code = await getCodeFromCortexLog(); 
-        await login(page); 
-        await connectRobot(page, code); 
-        await expect(page.getByText('Robotlocalhost')).toBeVisible();
+    test('Connect to robot in dev mode', async({page}) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('button', { name: 'Dev Mode' }).click();
+        await expect(page.getByText('Dev Mode127.0.0.1')).toBeVisible();
     });
 
     test('Disconnect from the robot', async({page}) => {
-        const startRes = await fetch('http://localhost:8080/auth/pair/start', {
-            method: 'POST',
-        });
-        const data = await startRes.json();
-        console.log('Expires in:', data.expires_in);
-        const code = await getCodeFromCortexLog(); 
-        await login(page); 
-        await connectRobot(page, code); 
-        await page.getByRole('button', { name: 'Connected: Robot' }).click();
-        await page.getByRole('button', { name: 'Disconnect' }).click(); 
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('button', { name: 'Dev Mode' }).click();
+        await page.getByRole('button', { name: 'Connected in Dev Mode' }).click();
+        await page.getByRole('button', { name: /disconnect/i }).click();
         await expect(page.getByText('Not connected')).toBeVisible();
     });
 
     test('Check going to app routes', async({page}) => {
-        await login(page); 
+        await page.goto('http://localhost:3000/');
         await page.getByRole('link', { name: 'Add from Apps' }).click();
         await expect(page).toHaveURL("http://localhost:3000/apps");
     }); 
 
+    test('Check going to console route', async({page}) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('link', { name: 'Console' }).click();
+        await expect(page.getByText('No robot connected. Use the')).toBeVisible();
+    }); 
+
+    test('Check going to console with robot connected', async({page}) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('button', { name: 'Dev Mode' }).click();
+        await page.getByRole('link', { name: 'Console' }).click();
+        await expect(page.getByText('#################################')).toBeVisible();
+    }); 
+
+
     // waiting to see how to correctly implement apps 
     /** 
     test('Check adding an app', async({page}) => {
-        const startRes = await fetch('http://localhost:8080/auth/pair/start', {
-            method: 'POST',
-        });
-        const data = await startRes.json();
-        console.log('Expires in:', data.expires_in);
-        const code = await getCodeFromCortexLog(); 
-        await login(page); 
-        await connectRobot(page, code); 
         await page.getByRole('link', { name: 'Add from Apps' }).click();
+        await page.getByText('No local container images').click();
         await page.getByRole('button', { name: 'Select version' }).click();
         await page.getByRole('button', { name: 'v0.0.1' }).click();
     });
 
     test('Check trying to add an app when the robot is not connected', async({page}) => {
-        await login(page); 
         await page.getByRole('link', { name: 'Add from Apps' }).click();
         await page.getByRole('button', { name: 'Select version' }).click();
         await page.getByRole('button', { name: 'v0.0.1' }).click();
@@ -76,7 +53,7 @@ test.describe.serial("Console tests", () => {
     
     */
     test('Incorrect ip given for connection', async({page}) => {
-        await login(page);  
+        await page.goto('http://localhost:3000/');
         await page.getByRole('button', { name: 'Connect' }).click();
         await page.getByRole('textbox', { name: 'e.g.' }).fill('l');
         await page.getByLabel('Connect to robot').getByRole('button', { name: 'Connect' }).click();
@@ -84,7 +61,7 @@ test.describe.serial("Console tests", () => {
     });
 
     test('Incorrect code given for connecting', async({page}) => {
-        await login(page); 
+        await page.goto('http://localhost:3000/');
         await page.getByRole('button', { name: 'Connect' }).click();
         await page.getByRole('textbox', { name: 'e.g.' }).fill('localhost');
         await page.getByLabel('Connect to robot').getByRole('button', { name: 'Connect' }).click();
